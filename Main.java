3435b288603b36c2a6a052ede9d456f725466d76
mjpetsalo <145345210+mjpetsalo@users.noreply.github.com>
2023-09-25 14:19:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,87 +13,88 @@ import java.util.Scanner;
 
 public class Main {
 
-	public ArrayList<Person> persons = new ArrayList<Person>();
-	
-	public static void main(String[] args) {
+    public static ArrayList<Person> persons = new ArrayList<Person>();
+
+    public static void main(String[] args) {
 
 
-		int choize;
-		
-		do {
-			
-			choize = readTheMenuAndWrite();
-			executeTheJob(choize);
-			
-			
-		}while(choize != 0);
-		
+        int choize;
 
-	}
-	
-	public static int readTheMenuAndWrite() {
-		
-		int choize;
-		
-		Scanner esk = new Scanner(System.in);
-		
-		System.out.println("1-load into");
-		System.out.println("2-load a relationship with someone");
-		System.out.println("3-print out someone");
-		//...
-		System.out.println("0-log out");
-		
-		choize = esk.nextInt();
-		
-		return choize;
+        do {
 
-		
-	}
-	
-	public void executeTheJob(int choize) {
-		
-		switch(choize) {
-		case 1:
+            choize = readTheMenuAndWrite();
+            executeTheJob(choize);
+
+            
+
+        }while(choize != 0);
 
 
+    }
+
+    public static int readTheMenuAndWrite() {
+
+        int choize;
+
+        Scanner esk = new Scanner(System.in);
+
+        System.out.println("1-load into");
+        System.out.println("2-load a relationship with someone");
+        System.out.println("3-print out someone");
+        //...
+        System.out.println("0-log out");
+
+        choize = esk.nextInt();
+
+        return choize;
 
 
+    }
 
+    public static void executeTheJob(int choize) {
 
-
-					try {
-						FileInputStream in = new FileInputStream("friends.txt");
-						Scanner s =new Scanner(in);
-
-						while(s.hasNextLine()){
-							persons.add(new Person(s.nextLine()));
-
-						}
-					}
-
-
-
-					catch(IOException e){
-						System.out.println("File not found");
-					}
+        switch(choize) {
+            case 1:
 
 
 
 
 
-		
-		break;
-		case 2:
-			
-		break;
-		case 3:
-			
-		break;
-		case 0:
-			System.out.println("Byebye, come back soon.");
-		break;
-		}
-		
-	}
+
+
+                try {
+                    FileInputStream in = new FileInputStream("/home/miro/IdeaProjects/dsa/DSAghub/src/friends.txt");
+                    Scanner s =new Scanner(in);
+
+                    while(s.hasNextLine()){
+                        persons.add(new Person(s.nextLine()));
+
+                    }
+                }
+
+
+
+                catch(IOException e){
+                    System.out.println("File not found");
+                }
+
+
+
+
+
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 0:
+                System.out.println("Byebye, come back soon.");
+                break;
+        }
+
+    }
 
 }
