897f24ac38a8eb40d103c3baa0ea4e4fdcc55eac
SSaaiizz <145345145+SSaaiizz@users.noreply.github.com>
2023-10-20 11:56:18 +0200

    Person.java

--- Person.java
+++ Person.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
+/**
+* The Person class gives the data so the object Person can be used in the main program, this object will work as an User
+*
+*@2023-10-20
+*@version 1.0
+*@author G612383
+*/
 public class Person {
 
     private String[] data;
@@ -20,16 +26,29 @@ public class Person {
     private String films;
     private String groupCode;*/
 
-
+        /**
+        *This method is used to separate and read each other of the  datas given in the parameter sent
+	*
+        *@param input is an String with different data separating each data by a "," 
+	*it does not retun any information
+	*/
     public Person(String input) {
        data= input.split(",");
 
     }
-    
+        /**
+	*This method is used to get the different data we got in the method above, so we can use it later
+	*
+	*@return data this returns a String[] with the different data we got in the method above
+	*/
      public String[] getData(){
         return data;
     }
-
+        /*
+	*This methods objective is to get an ArrayList so we can use it later on
+	*
+        *@return list this returns a ArrayList<String> we may use in other methods
+	*/
 	public ArrayList<String> getList() {
 		return list;
 	}
